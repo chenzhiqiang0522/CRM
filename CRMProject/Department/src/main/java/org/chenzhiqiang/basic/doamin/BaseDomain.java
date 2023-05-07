c39e3527e26f0dae6966719466e2531d6728c6a5
NickWiber <1249323612@qq.com>
2023-05-07 17:25:34 +0800

    自动生成Sys代码

---
+++ CRMProject/Department/src/main/java/org/chenzhiqiang/basic/doamin/BaseDomain.java
@@ -0,0 +1,13 @@
+package org.chenzhiqiang.basic.doamin;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseDomain {
+    private Long id;
+
+}
--- CRMProject/Department/src/test/java/org/chenzhiqiang/services/impl/DepartmentServiceImplTest.java
+++ CRMProject/Department/src/test/java/org/chenzhiqiang/services/impl/DepartmentServiceImplTest.java
@@ -11,37 +11,37 @@ public class DepartmentServiceImplTest extends BaseTest {
     @Autowired
     private DepartmentServiceImpl departmentService;
 
-    @Test
-    public void getAllDepartment() {
-        List<Department> allDepartments = departmentService.getAllDepartments();
-        allDepartments.forEach(System.out::println);
-    }
-
-    @Test
-    public void selectDepartmentByID() {
-        Department department = departmentService.selectDepartmentByID(45L);
-        System.out.println(department);
-    }
-
-    @Test
-    public void deleteDepartmentByName() {
-        Integer integer = departmentService.deleteDepartmentByName("utils");
-    }
-
-    @Test
-    public void addDepartment() {
-        Department department = new Department("utils", "this s a   jj k k  l  k uj i ");
-        departmentService.addDepartment(department);
-        System.out.println("返回的id为："+department.getId());
-    }
-
-    @Test
-    public void deleteDepartmentById() {
-        Integer integer = departmentService.deleteDepartmentById(52L);
-    }
-
-    @Test
-    public void modifyDepartment() {
-        departmentService.modifyDepartment(new Department("utils22", "2222222222222222"));
-    }
+//    @Test
+//    public void getAllDepartment() {
+//        List<Department> allDepartments = departmentService.getAllDepartments();
+//        allDepartments.forEach(System.out::println);
+//    }
+//
+//    @Test
+//    public void selectDepartmentByID() {
+//        Department department = departmentService.selectDepartmentByID(45L);
+//        System.out.println(department);
+//    }
+//
+//    @Test
+//    public void deleteDepartmentByName() {
+//        Integer integer = departmentService.deleteDepartmentByName("utils");
+//    }
+//
+//    @Test
+//    public void addDepartment() {
+//        Department department = new Department("utils", "this s a   jj k k  l  k uj i ");
+//        departmentService.addDepartment(department);
+//        System.out.println("返回的id为："+department.getId());
+//    }
+//
+//    @Test
+//    public void deleteDepartmentById() {
+//        Integer integer = departmentService.deleteDepartmentById(52L);
+//    }
+//
+//    @Test
+//    public void modifyDepartment() {
+//        departmentService.modifyDepartment(new Department("utils22", "2222222222222222"));
+//    }
 }
