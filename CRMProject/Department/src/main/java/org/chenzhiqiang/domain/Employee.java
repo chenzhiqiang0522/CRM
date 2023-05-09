ba311a3e8566facbabdbb6108fe10e5020735d82
NickWiber <1249323612@qq.com>
2023-05-09 11:03:55 +0800

    删除库文件

--- CRMProject/Department/src/main/java/org/chenzhiqiang/domain/Employee.java
+++ CRMProject/Department/src/main/java/org/chenzhiqiang/domain/Employee.java
@@ -1,6 +1,5 @@
 package org.chenzhiqiang.domain;
 
-import com.oracle.webservices.internal.api.databinding.DatabindingMode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
